180133b60f14a4e17b5eb8ffb33435762ec7ecef
Brian Alexander <brianalexander348@gmail.com>
2025-10-19 23:28:47 +0700

    add route for find and update

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { createUser } = require("../controllers/userControllers");
+const { createUser, findUser, updateUser } = require("../controllers/userControllers");
 
 const router = express.Router();
 
 // POST /users
 router.post("/", createUser);
+router.get('/find', findUser);
+router.put('/update/:id', updateUser);
 
 module.exports = router;
 
